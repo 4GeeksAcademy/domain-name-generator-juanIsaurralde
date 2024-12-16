25437c4c9a39b52ff3e582e4d68444a2265f9311
JuanMIsaurralde <isaurraldejuanm@gmail.com>
2024-12-16 17:34:57 +0000

    [sp-93] feat: delay

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dotX = [".com", ".net", ".us", ".io"];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  for (const pronounEl of pronoun) {
+    for (const adjEl of adj) {
+      for (const nounEl of noun) {
+        for (const dotXEl of dotX) {
+          console.log(pronounEl + adjEl + nounEl + dotXEl);
+        }
+      }
+    }
+  }
 };
